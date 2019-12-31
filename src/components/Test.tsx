700741c116d1815c76e8276f8f8f160bdd14efa4
Daniel <daniel@bezinga.com>
2019-12-31 08:58:08 +0800

    Setup React Router

---
+++ src/components/Test.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+
+class Test extends React.Component {
+  render() {
+    return <h1>Users</h1>
+  }
+}
+export default Test
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,26 @@
-import * as React from "react";
-import {render} from "react-dom";
-import App from "./components/App";
+import * as React from 'react'
+import { render } from 'react-dom'
+import App from './components/App'
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import Test from './components/Test'
 
-const rootEl = document.getElementById("root");
+const rootEl = document.getElementById('root')
 
-render(
-    <App/>,
-    rootEl,
-);
+const routing = (
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/test">Test</Link>
+        </li>
+      </ul>
+      <Route exact path="/" component={App} />
+      <Route path="/test" component={Test} />
+    </div>
+  </Router>
+)
+
+render(routing, rootEl)
